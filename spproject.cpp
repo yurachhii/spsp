3a34ebcff1e09fbcca94d673c83f9da7407c7976
mariam <mariamayman23hh@gmail.com>
2025-03-17 11:31:38 +0200

    delete file

---
+++ spproject.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main (){
+    cout<<"Hello world\n";
+    cout<<"Final\n";
+    cout<<"hahahah\n";
+    
+   
+}
